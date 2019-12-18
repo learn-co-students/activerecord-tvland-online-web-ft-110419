fbf956718271365a2df9835aa3c0702d7b1e6b5a
ncaudill27 <nelson.a.caudill@gmail.com>
2019-12-18 01:08:40 -0500

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,13 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :actor
+  belongs_to :show
+
+  def say_that_thing_you_say
+    "#{name} always says: #{catchphrase}"
+  end
+
+  def build_show(args)
+    show = Show.find_by(args)
+    self.show = show || self.show = Show.create(args)
+  end
 end
