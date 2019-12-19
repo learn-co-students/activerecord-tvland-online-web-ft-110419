46091383b9586e343b08bb9afac6e664b3abdf17
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-12-19 23:18:20 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,10 @@
 class Show < ActiveRecord::Base
-  
-end
+  has_many :characters
+   has_many :actors, through: :characters
+   belongs_to :network
+   def actors_list
+     self.actors.map do |a|
+       a.full_name
+     end
+   end
+ end
