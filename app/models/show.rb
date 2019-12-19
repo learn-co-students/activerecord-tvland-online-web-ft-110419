47541280b72cfdcb7aa8f579c3da91764b1d89fd
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-12-19 21:13:30 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
+
 class Show < ActiveRecord::Base
-  
+
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list #has an #actors_list method that returns a list of the full names of each actor associated with the show
+    self.actors.collect do |actors|
+      actors.full_name
+   end
+  end
 end
