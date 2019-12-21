58b08442665c8750e82077501bbb09e5ebb1afe0
Jesse Gonzalez <jessegonzalez024@gmail.com>
2019-12-21 02:04:23 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
 class Show < ActiveRecord::Base
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
   
+  def build_network(args)
+    network = Network.find_by(args)
+    self.network = network || self.network = Network.create(args)
+  end
+  
+  def actors_list
+    actors.collect{|actor| actor.full_name}
+  end
 end
