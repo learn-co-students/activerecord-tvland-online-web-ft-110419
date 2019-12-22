6de93cc31d5a367a3c5eae9969f3465cf0cc067d
kjdowns <kevin.downs04@gmail.com>
2019-12-22 10:40:16 -0500

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,16 @@
 class Actor < ActiveRecord::Base
+  has_many :characters
+  has_many :shows, through: :characters
   
+  def full_name
+    string = []
+    string << self.first_name
+    string << self.last_name
+    string.join(" ")
+  end
+
+  def list_roles
+    self.characters.map{|character| "#{character.name} - #{character.show.name}"}
+  end
+
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
+require 'pry'
 class Show < ActiveRecord::Base
-  
+  has_one :network
+  has_many :characters
+  has_many :actors, through: :characters
+
+  def actors_list
+    self.actors.map{|actor| actor.full_name}
+  end
+
+  def build_network(hash)
+    Network.find_or_create_by(hash).shows << self
+  end
 end
